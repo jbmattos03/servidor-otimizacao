bbfcf726f322872b8df8cfb0f24725076275c5d6
jbmattos03 <jbmattos2003@gmail.com>
2025-05-04 12:02:08 -0300

    fix: :bug: Fix some QuestionService functions
    Updated newQuestion to receive new Question attributes; Fixed getQuestionById; Refactored getAnsweredQuestionsByUser, deleteQuestion and answerQuestion to use functions already existent in QuestionService

--- src/Service/QuestionService.js
+++ src/Service/QuestionService.js
@@ -2,10 +2,12 @@ import Question from "../Models/QuestionModel.js";
 import callPythonFunction from "../Middleware/PythonFunction.js";
 
 class QuestionService {
-    static async newQuestion(user, question, answer) {
+    static async newQuestion(user, qtd_var_obj, qtd_res_des, matriz, answer) {
         const newQuestion = await Question.create({
             user,
-            question,
+            qtd_var_obj,
+            qtd_res_des,
+            matriz,
             answer,
             status: 0,
         });
@@ -14,7 +16,7 @@ class QuestionService {
     }
 
     static async getQuestionById(questionId) {
-        const question = await Question.findById(questionId);
+        const question = await Question.findByPk(questionId);
 
         if (!question) {
             throw new Error("Question not found");
@@ -34,34 +36,34 @@ class QuestionService {
     }
 
     static async getAnsweredQuestionsByUser(id) {
-        const questions = await Question.findAll({where: {user: id, status: 1}});
-
-        if(!questions) {
-            throw new Error("No questions found");
-        }
+        const questions = await QuestionService.getAllQuestionsByUser(id);
 
         return questions;
     }
 
     static async deleteQuestion(questionId) {
-        const question = await Question.findById(questionId);
+        const question = await QuestionService.getQuestionById(questionId);
 
-        if (!question) {
-            throw new Error("Question not found");
-        }
-
-        await Question.destroy({where: {id: questionId}});
+        await Question.destroy({ where: { id: questionId } });
     }
 
     static async answerQuestion(questionId) {
-        const question = await Question.findById(questionId);
+        const question = await QuestionService.getQuestionById(questionId);
 
-        if (!question) {
-            throw new Error("Question not found");
-        }
+        //if (question.isAnswered === 1) {
+           // throw new Error("Question already answered");
+        //}
 
-        const answer = await callPythonFunction(question.values.question);
-        const answeredQuestion = await Question.update({answer: answer, status: 1}, {where: {id: questionId}});
+        const answer = await callPythonFunction({
+            qtd_var_obj: question.qtd_var_obj,
+            qtd_res_des: question.qtd_res_des,
+            matriz: question.matriz,
+        });
+
+        const answeredQuestion = await Question.update(
+            { answer: answer, isAnswered: 1 }, 
+            { where: { id: questionId } }
+        );
 
         return answeredQuestion;
     }
