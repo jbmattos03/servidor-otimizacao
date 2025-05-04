c225f0bedce474009389e3f47544a3aacdb461eb
jbmattos03 <jbmattos2003@gmail.com>
2025-05-04 11:54:57 -0300

    refactor: :fire: Remove lJWT related logs for security reasons

--- src/Middleware/Auth.js
+++ src/Middleware/Auth.js
@@ -6,21 +6,12 @@ function auth(req, res, next) {
         console.log('Auth Header:', req.header("Authorization"));
 
         const token = req.header("Authorization")?.replace("Bearer ", "");
-        
-        // Log the extracted token
-        console.log('Extracted Token:', token);
 
         if (!token) {
             return res.status(401).json({ message: "Access denied. No token provided." });
         }
 
-        // Log the secret key being used (be careful with this in production)
-        console.log('Using JWT Secret:', process.env.JWT_SECRET);
-
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        
-        // Log the decoded token
-        console.log('Decoded Token:', decoded);
         
         req.user = decoded;
         next();
